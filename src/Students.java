06761c14463bf26cb7d9fc9203a3050c3c7891c2
kashmira29 <kashmirapandar@gmail.com>
2019-07-29 11:10:54 +0100

    first commit

---
+++ src/Students.java
@@ -0,0 +1,49 @@
+public class Students {
+    public static void main (String args[]){
+        String name = "Shanaya";
+        System.out.println(name);
+        String devision = "A";
+        System.out.println(devision);
+        String nursery_name = "Hungry Caterpiller";
+        System.out.println(nursery_name);
+        double schoolTime = 6.00;
+        System.out.println(schoolTime);
+        int homeNo = 51;
+        System.out.println(homeNo);
+        double schoolFinish = 2.00;
+        System.out.println(schoolFinish);
+        String fevouriteFruit = "grapes";
+        System.out.println(fevouriteFruit);
+        String hobby = "music";
+        System.out.println(hobby);
+        boolean fevouriteHobbySwimming = true;
+        System.out.println(fevouriteHobbySwimming);
+        int shoesize = 5;
+        System.out.println(shoesize);
+        double sandlesize = 5.5;
+        System.out.println(sandlesize);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
